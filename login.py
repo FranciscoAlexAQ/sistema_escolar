89c682a2b2acaead9ad6ba8ad68220021b59c9e7
Francisco Alex <franciscoalexlq@gmail.com>
2021-10-17 18:30:37 -0300

    criando gitignore e estilizando botao de login

--- login.py
+++ login.py
@@ -1,42 +1,38 @@
+# importações
 from tkinter import Tk, Toplevel
 from tkinter import Button, Entry, Label
 from tkinter import messagebox
-from tkinter.constants import ANCHOR, CENTER
+from tkinter.constants import CENTER
 from tkinter.ttk import Treeview 
 from PIL import ImageTk, Image 
 
 
+# classe principal
 class Login:
     def __init__(self, master) -> None:
-        if __name__ != 'main':
-            self.janela = Toplevel(master)
+        self.janela = Toplevel(master)
 
-            self.configuar()
-            self.inserirImagens()
-            self.criarLabel()
-            self.criarEntrys()
-            self.criarBotoes()
+        # chamando métodos
+        self.configuar()
+        self.inserirImagens()
+        self.criarLabel()
+        self.criarEntrys()
+        self.criarBotoes()
 
-            self.janela.transient(master)
-        else:
-            self.janela = Tk()
-
-            self.configuar()
-            self.inserirImagens()
-            self.criarLabel()
-            self.criarEntrys()
-            self.criarBotoes()
-
-            self.janela.mainloop()
-
+        self.janela.transient(master)
+        
 
      # método para configuração da tela principal
     def configuar(self) -> None:
         self.janela.title('Login')
         self.janela.geometry('400x350')
         self.janela.resizable(False, False)
+        
 
+    # método para mudar o tamanho das imagens
     def redimencionarImagem(self, width, caminho):
+        global imagemUser
+
         img = Image.open(caminho)
         width = width
 
@@ -45,15 +41,17 @@ class Login:
 
         img = img.resize((width, hsize), Image.ANTIALIAS)
 
-        self.imagemUser = ImageTk.PhotoImage(img)
+        imagemUser = ImageTk.PhotoImage(img)
 
-        return self.imagemUser
+        return imagemUser
 
+    # método para inserir imagens
     def inserirImagens(self):
         self.foto = self.redimencionarImagem(100, 'images/user.png')
         self.labelFoto = Label(self.janela, image=self.foto)
         self.labelFoto.place(relx=0.39, rely=0.05)
 
+    # método para criar labels
     def criarLabel(self):
         self.labelUser = Label(self.janela, text='Usuário')
         self.labelUser.place(relx=0.1, rely=0.43)
@@ -61,6 +59,7 @@ class Login:
         self.labelSenha = Label(self.janela, text='Senha')
         self.labelSenha.place(relx=0.1, rely=0.59)
 
+    # método para criar entries
     def criarEntrys(self):
         self.entryUser = Entry(self.janela, justify=CENTER)
         self.entryUser.place(relx=0.3, rely=0.4, relheight=0.1)
@@ -68,8 +67,7 @@ class Login:
         self.entrySenha = Entry(self.janela, justify=CENTER)
         self.entrySenha.place(relx=0.3, rely=0.57, relheight=0.1)
 
-
+    # método para criar botões
     def criarBotoes(self):
-        self.btnLogar = Button(self.janela, text='Fazer Login')
+        self.btnLogar = Button(self.janela, text='Fazer Login', bg='#FF8C00', fg='white')
         self.btnLogar.place(relx=0.3, rely=0.74, relwidth=0.4, relheight=0.12)
-
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# importações
-from tkinter import Tk
-from tkinter import Button, Entry, Label
-from tkinter import messagebox
-from tkinter.ttk import Treeview 
+# importações de módulos
+from tkinter import Tk, font
+from tkinter import Button, Label
 from PIL import ImageTk, Image 
-import login 
 
+# importações de arquivos do programa
+import login 
 
 # instanciando tkinter
 root = Tk()
 
 
+# classe principal
 class Main:
     def __init__(self) -> None:
         # referenciando
@@ -31,25 +31,26 @@ class Main:
         self.root.resizable(False, False)
         
     # método para pegar as imagens 
-    def inserirImagens(self):
+    def inserirImagens(self) -> None:
         self.fotoFundo = ImageTk.PhotoImage(Image.open('images/fundo.jpg'))
         self.labelFundo = Label(self.root, image=self.fotoFundo)
         self.labelFundo.place(relx=0, rely=-0, relwidth=1, relheight=1)
     
     # criar botões 
-    def criarBotoes(self):
+    def criarBotoes(self) -> None:
         self.btnEstudantes = Button(self.root, text='Estudantes', bg='#FF8C00', fg='white', bd=0, 
-            activebackground='#FF4500', activeforeground='white', command=lambda: login.Login(self.root))
+            activebackground='#FF4500', activeforeground='white', 
+            command=lambda: login.Login(self.root))
         self.btnEstudantes.place(relx=0.01, rely=0.05, relwidth=0.14, relheight=0.1)
 
         self.btnProfessores = Button(self.root, text='Professores', bg='#DC143C', fg='white', bd=0, 
-            activebackground='#B22222', activeforeground='white')
+            activebackground='#B22222', activeforeground='white', command=lambda: login.Login(self.root))
         self.btnProfessores.place(relx=0.01, rely=0.2, relwidth=0.14, relheight=0.1)
 
         self.btnDirecao = Button(self.root, text='Direção', bg='#483D8B', fg='white', bd=0, 
-            activebackground='#4B0082', activeforeground='white',)
+            activebackground='#4B0082', activeforeground='white', command=lambda: login.Login(self.root))
         self.btnDirecao.place(relx=0.01, rely=0.35, relwidth=0.14, relheight=0.1)
-        
-        
+     
+
 # chamando classe
 Main()
